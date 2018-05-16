988aee164dcfb17c75ff04ad5edcef2be18995e5
olgabot <olga.botvinnik@gmail.com>
2018-05-15 17:19:25 -0700

    Only output best barcode match and write barcode+primer info to csv

--- find_barcodes/main.go
+++ find_barcodes/main.go
@@ -2,12 +2,17 @@ package main
 
 import (
   "os"
+  "log"
   "github.com/biogo/biogo/seq/linear"
   "github.com/biogo/biogo/alphabet"
   "github.com/biogo/biogo/io/seqio"
   "fmt"
   "github.com/biogo/biogo/io/seqio/fasta"
   "github.com/biogo/biogo/io/seqio/fastq"
+  "strings"
+  "sort"
+  "encoding/csv"
+  "strconv"
 )
 
 // Hamming computes the hamming distance between the primer string and the read
@@ -52,8 +57,8 @@ func main() {
 
   primer_seqs := make([]*linear.Seq, 0)
 
-  barcode_start := 4
-  barcode_end := 20
+  barcode_start := 6
+  barcode_end := 15
 
   // Open the output file for writing:
   fho, err := os.Create(os.Args[3])
@@ -67,7 +72,14 @@ func main() {
 
   for primer_scanner.Next(){
     seq := primer_scanner.Seq().(*linear.Seq)
-	primer_seqs = append(primer_seqs, seq)
+
+    // Strip all Ns from left and right
+    seqString := seq.Seq.String()
+    stripped := strings.Trim(seqString, "N")
+    letters := []alphabet.Letter(stripped)
+    newSeq := linear.NewSeq(seq.ID, letters, seq.Alphabet())
+	primer_seqs = append(primer_seqs, newSeq)
+	//fmt.Fprintf(os.Stderr,"seq.Seq: %s\t newSeq.Seq: %s\n", seq.Seq, newSeq.Seq)
   }
 
 
@@ -76,6 +88,8 @@ func main() {
 
   //var i uint64 = 0
   var readID string
+  barcodeLengthCount := make(map[int]int)
+  primerCount := make(map[string]int)
 
 
   for read_scanner.Next(){
@@ -83,30 +97,117 @@ func main() {
 	annotation := read.CloneAnnotation()
 	readID = annotation.ID
 
+	bestDistance := read.Len()
+	var bestPrimer linear.Seq
+	var bestJ int
+
 	for i := range(primer_seqs) {
 	  primer := primer_seqs[i]
 	  for j := barcode_start ; j < barcode_end ; j++ {
 		distance := Hamming(primer, read, j)
-		if distance <= 1 {
-		  fmt.Println("Hamming distance for", annotation.ID, "+", primer.Seq, ":", distance)
-		  //  barcode := read.Seq[0:(j-1)].L
-		//  addition := strings.Join("BARCODE:"
-		//  // Something like this
-		//  id_parts := []string{annotation.ID, replaced}
-		//  new_id := strings.Join(id_parts, " ")
-		//  seq_replaced := linear.NewQSeq(new_id, read.Seq, read.Alphabet(), read.Encode)
-		//  writer.Write(seq_replaced)
+		if distance <= bestDistance {
+		  bestDistance = distance
+		  bestPrimer = *primer
+		  bestJ = j
 		}
 	  }
 	}
+	//fmt.Printf("%s\t%s\t%s\thamming: %d\tindex (j): %d\n", annotation.ID, bestPrimer.Seq, bestPrimer.ID, bestDistance, bestJ)
+
+	barcodeLengthCount[bestJ]++
+	primerCount[bestPrimer.ID]++
+	//fmt.Println(annotation.ID, "+", bestPrimer.Seq, bestPrimer.ID, "hamming distance:", bestDistance, "index (j):", bestJ)
+
+	writeBarcodeInfo(os.Args[1] + ".barcodeInfo.csv", barcodeLengthCount)
+	writePrimerInfo(os.Args[1] + ".primerInfo.csv", primerCount)
+
+	//  barcode := read.Seq[0:(j-1)].L
+	//  addition := strings.Join("BARCODE:"
+	//  // Something like this
+	//  id_parts := []string{annotation.ID, replaced}
+	//  new_id := strings.Join(id_parts, " ")
+	//  seq_replaced := linear.NewQSeq(new_id, read.Seq, read.Alphabet(), read.Encode)
+	//  writer.Write(seq_replaced)
 
   }
+
+
 
   err = read_scanner.Error()
   // handle errors
   if err != nil {
-    fmt.Println("last read before error:", readID)
+    fmt.Fprintln(os.Stderr,"last read before error:", readID)
 	panic(err)
   }
 
+}
+
+func writeBarcodeInfo(csvFileName string, barcodeLengthCount map[int]int){
+  // Create a separate, sorted list of barcode counts so the order is always the same
+  var barcodeLengths []int
+  for barcodeLength := range barcodeLengthCount {
+	barcodeLengths = append(barcodeLengths, barcodeLength)
+  }
+  sort.Ints(barcodeLengths)
+
+  // --- Write barcode info to file ---
+  csvFile, csvErr := os.Create(csvFileName)
+  if csvErr != nil {
+	panic(csvErr)
+  }
+  defer csvFile.Close()
+
+  csvWriter := csv.NewWriter(csvFile)
+  defer csvWriter.Flush()
+  header := []string{"barcode_length", "count"}
+  err := csvWriter.Write(header)
+  checkError("Cannot write to file", err)
+
+  for _, barcodeLength := range barcodeLengths {
+	//fmt.Fprintln(os.Stderr,"Barcode Length:", barcodeLength, "\tCount:", barcodeLengthCount[barcodeLength])
+	data := []string{strconv.Itoa(barcodeLength),
+	  strconv.Itoa(barcodeLengthCount[barcodeLength])}
+	//line := stringify(data)
+	err := csvWriter.Write(data)
+	checkError("Cannot write to file", err)
+  }
+}
+
+func writePrimerInfo(csvFileName string, primerCount map[string]int){
+
+  // --- Write primer info to file ---
+  csvFile, csvErr := os.Create(csvFileName)
+  if csvErr != nil {
+	panic(csvErr)
+  }
+  defer csvFile.Close()
+
+  csvWriter := csv.NewWriter(csvFile)
+  defer csvWriter.Flush()
+  header := []string{"barcode_length", "count"}
+  err := csvWriter.Write(header)
+  checkError("Cannot write to file", err)
+
+  for primerID, count := range primerCount {
+	//fmt.Fprintf(os.Stderr,"%s\t%d\n", primerID, count)
+	data := []string{primerID, strconv.Itoa(count)}
+	//line := stringify(data)
+	err := csvWriter.Write(data)
+	checkError("Cannot write to file", err)
+  }
+}
+
+//func stringify(intArray []int) []string {
+//  stringArray := make([]string, len(intArray))
+//  for i, v := range intArray {
+//	stringArray[i] += strconv.Itoa(v)
+//  }
+//
+//  return stringArray
+//}
+
+func checkError(message string, err error) {
+  if err != nil {
+	log.Fatal(message, err)
+  }
 }
